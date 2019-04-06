0ed5d9786f0ef63eddee481797efd383e82b92e9
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-04-06 22:02:40 +0530

    Taking Input with Initialize function Ready

--- COA.cpp
+++ COA.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 //Functions 
 void Initialize();
+void FillMainMemory();
 void PrintTable();
 void DirectM();
 void AssociativeM();
@@ -20,13 +21,46 @@ int missDM,missAM,missSM;
 
 // Main Function for taking Inputs 
 int main (){
-	// Code
+
+	// Take input for Creating Cache Memory
+	cout<<"\t\t\tWelcome to Cache Memory Mapping Simulator\n";
+	cout<<"\nEnter no. of lines in the Cache Memory: ";
+	cin>>noOfLines;
+	cout<<"\nEnter no. of Sets in Set Associative Mapping: ";
+	cin>>noOfSet;
+
+	Initialize();
+
+	FillMainMemory();
+
 	return 0;
 }
 
-// initializes Parameters to default value 
+// Initializes Parameters to default value 
 void Initialize(){
-	// Code
+
+	int i;
+	// Variable initialized to zero
+	timeForDM = 0; timeForAM = 0; timeForSM = 0;
+	hitDM =0; hitAM = 0; hitSM = 0;
+	missDM = 0; missAM = 0; missSM = 0;
+
+	// Array initialized to zero
+	for( i = 0; i < 50; i++)
+	{
+		directM[i] = 0; associativeM[i] = 0; setM[i] = 0;
+	}
+	for( i = 0; i < 100; i++)
+	{
+		mainM[i] = 0;
+	}
+
+}
+
+// Fills Values into main memory
+void FillMainMemory(){
+	cout<<"\t\t\tMemory is Restricted from 0 to 99 Blocks Only\n";
+	cout<<"Pass input in the given format Block No. "
 }
 
 // Print Table of Memory visualization 
