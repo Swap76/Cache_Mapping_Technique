962cd6e048ed0a149cc5876dd4bce2dda886fe43
Swap76 <swapnil.satish17@siesgst.ac.in>
2019-04-06 21:18:11 +0530

    Boiler Plate Completed

---
+++ COA.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Functions 
+void Initialize();
+void PrintTable();
+void DirectM();
+void AssociativeM();
+void SetM();
+
+//Input Declearation 
+int directM[50],associativeM[50],setM[50],mainM[100];
+int timeToReadFromCache,timeToReadFromMemory;
+int noOfLines,noOfSet;
+
+//Output Declearation 
+int timeForDM,timeForAM,timeForSM;
+int hitDM,hitAM,hitSM;
+int missDM,missAM,missSM;
+
+// Main Function for taking Inputs 
+int main (){
+	// Code
+	return 0;
+}
+
+// initializes Parameters to default value 
+void Initialize(){
+	// Code
+}
+
+// Print Table of Memory visualization 
+void PrintTable(){
+    // Code
+}
+
+// Calculation for Direct Mapping 
+void DirectM(){
+    // Code
+}
+
+// Calculation for Associative Mapping 
+void AssociativeM(){
+	// Code
+}
+
+// Calculation for Set Associative Mapping
+void SetM(){
+	// Code
+}
